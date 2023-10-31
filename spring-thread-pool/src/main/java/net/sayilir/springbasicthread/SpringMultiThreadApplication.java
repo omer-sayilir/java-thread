3d09977701b48d5d329421efd4e11e03983f2b38
omers <omer.sayilir@cfn.com.tr>
2023-11-01 00:31:52 +0300

    multithread cached pool

--- spring-thread-pool/src/main/java/net/sayilir/springbasicthread/SpringMultiThreadApplication.java
+++ spring-thread-pool/src/main/java/net/sayilir/springbasicthread/SpringMultiThreadApplication.java
@@ -11,17 +11,15 @@ import java.util.concurrent.Executors;
 @SpringBootApplication
 public class SpringMultiThreadApplication {
     public static void main(String[] args){
-        ExecutorService executorService= Executors.newFixedThreadPool(10);
-        for (int i=0; i<10;i++){
+        ExecutorService executorService= Executors.newCachedThreadPool();
+        for (int i=0; i<40;i++){
             RunnableWorker runnableWorker = new RunnableWorker("worker "+i);
             executorService.execute(runnableWorker);
         }
-        executorService.shutdown();
+
         SpringApplication.run(SpringMultiThreadApplication.class,args);
 
-        while(!executorService.isTerminated()){
-            //wait
-        }
+
         System.out.println("All thread completed");
     }
 }
